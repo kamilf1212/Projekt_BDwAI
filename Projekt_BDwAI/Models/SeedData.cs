16a1b496eacea5b7330dcb695f8982d4deb6dced
Jan Goluch <s168733@student.uken.krakow.pl>
2025-12-15 18:21:57 +0100

    Przeniesienie seed admina do SeedData z Program.cs

---
+++ Projekt_BDwAI/Models/SeedData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Projekt_BDwAI.Areas.Identity.Data;
+
+namespace Projekt_BDwAI.Models
+{
+    public class SeedData
+    {
+        public static async void Initialize(IServiceScope scope)
+        {
+            var services = scope.ServiceProvider;
+
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var roles = new[] { "Admin", "User" };
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+            string email = "admin@admin.com";
+            string password = "Admin123!";
+            if (await userManager.FindByEmailAsync(email) == null)
+            {
+                var adminUser = new User
+                {
+                    FirstName = "Jan",
+                    LastName = "Kowalski",
+                    Email = email,
+                    UserName = email
+                };
+                await userManager.CreateAsync(adminUser, password);
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+        }
+    }
+}
--- Projekt_BDwAI/Program.cs
+++ Projekt_BDwAI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Projekt_BDwAI.Areas.Identity.Data;
+using Projekt_BDwAI.Models;
 
 namespace Projekt_BDwAI
 {
@@ -28,7 +29,6 @@ namespace Projekt_BDwAI
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -49,37 +49,13 @@ namespace Projekt_BDwAI
             app.MapRazorPages();
             app.MapStaticAssets();
 
-            // Seed roles and admin user
+            // Ensure DB schema is up-to-date and run seeding (awaited)
             using (var scope = app.Services.CreateScope())
             {
-                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-                var roles = new[] { "Admin", "User" };
-                foreach (var role in roles)
-                {
-                    if (!await roleManager.RoleExistsAsync(role))
-                    {
-                        await roleManager.CreateAsync(new IdentityRole(role));
-                    }
-                }
-            }
+                var db = scope.ServiceProvider.GetRequiredService<Project_context>();
+                await db.Database.MigrateAsync(); // apply migrations
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
-                string email = "admin@admin.com";
-                string password = "Admin123!";
-                if (await userManager.FindByEmailAsync(email) == null)
-                {
-                    var adminUser = new User
-                    {
-                        FirstName = "Jan",
-                        LastName = "Kowalski",
-                        Email = email,
-                        UserName = email
-                    };
-                    await userManager.CreateAsync(adminUser, password);
-                    await userManager.AddToRoleAsync(adminUser, "Admin");
-                }
+                await Task.Run(() => SeedData.Initialize(scope));
             }
 
             app.Run();
